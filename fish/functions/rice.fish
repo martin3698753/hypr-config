ddba2b6fd04d67a89c0dff42a6328cd6ec72138c
martin3698753 <martinek8425@gmail.com>
2025-09-14 17:28:34 +0200

    added pics

--- fish/functions/rice.fish
+++ fish/functions/rice.fish
@@ -1,4 +1,4 @@
 function rice -d "List of rice files"
-    git add dunst fish hypr kitty nvim sway swaylock waybar wlogout wofi README.md Makefile
+    git add dunst fish hypr kitty nvim sway swaylock waybar wlogout wofi README.md Makefile centipede.png tk1.jpg tk2.jpg wallpaper-fedora.jpg
     echo "rice: added git files"
 end
